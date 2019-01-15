60739f4c6e6103e319e8f9b4b7b993a78f3e451f
clevernyyyy <adam.t.schaal@gmail.com>
2019-01-15 12:31:43 -0600

    updated CoC to use DEF CON CoC

--- src/components/CodeOfConduct/CodeOfConduct.js
+++ src/components/CodeOfConduct/CodeOfConduct.js
@@ -9,9 +9,25 @@ class CodeOfConduct extends Component {
       <div>
         <div className='tab-title'>Code Of Conduct</div>
         <div className='code-of-conduct-text'>
+          <p>Kernelcon provides a forum for open discussion between participants, where radical viewpoints are welcome and a high degree of skepticism is expected. However, insulting or harassing other participants is unacceptable. We want Kernelcon to be a safe and productive environment for everyone. It's not about what you look like but what's in your mind and how you present yourself that counts at Kernelcon.</p>
+          <p>We do not condone harassment against any participant, for any reason. Harassment includes deliberate intimidation and targeting individuals in a manner that makes them feel uncomfortable, unwelcome, or afraid.</p>
+          <p>Participants asked to stop any harassing behavior are expected to comply immediately. We reserve the right to respond to harassment in the manner we deem appropriate, including but not limited to expulsion without refund and referral to the relevant authorities.</p>
+          <p>This Code of Conduct applies to everyone participating at Kernelcon - from attendees and exhibitors to speakers, press, volunteers, and Kernelcon Crew.</p>
+          <p>Anyone can report harassment. If you are being harassed, notice that someone else is being harassed, or have any other concerns, you can contact a Kernelcon Crew member, go to the registration desk, or info booth.</p>
+          <p>Conference staff will be happy to help participants contact hotel security, local law enforcement, or otherwise assist those experiencing harassment to feel safe for the duration of Kernelcon.</p>
+          <p>Remember: The CON is what you make of it, and as a community we can create a great experience for everyone.</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CodeOfConduct;
+
+{/*
+
           <p>Kernelcon is committed to providing a harassment-free experience for every person, regardless of gender, gender identity and expression, sexual orientation, disability, physical appearance, body size, age, race, religion, or editor preference. We do not tolerate any forms of harassment of group participants.</p>
           <p>This code of conduct applies to all Kernelcon mediums, including meetups, events, and online chats. Anyone who violates this code of conduct may be sanctioned or expelled from these mediums at the discretion of the Kernelcon BODs.</p>
-
 
           <h4>Harassment includes:</h4>
           <ul>
@@ -39,17 +55,5 @@ class CodeOfConduct extends Component {
             <li>Restrict conversation of sensitive subjects including, but not limited to, politics and religion, to private communications.</li>
             <li>If you are being harassed by a Kernelcon member, notice that someone else is being harassed, or have any other concerns, please contact the Kernelcon BODs at report@kernelcon.org. If the person who is harassing you is a Kernelcon BOD, they will recuse themselves from handling your incident. We will respond in a prompt and decisive manner.</li>
           </ul>
-        </div>
-      </div>
-    );
-  }
-}
 
-export default CodeOfConduct;
-
-
-
-
-
-
-
+*/}
