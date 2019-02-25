57052c582a8c76bdd396407e4d5f01f31f062853
clevernyyyy <adam.t.schaal@gmail.com>
2019-02-25 00:20:28 -0600

    removing ability to sign up to volunteer

--- src/pages/Registration/Registration.js
+++ src/pages/Registration/Registration.js
@@ -1,31 +1,12 @@
 import React, { Component } from 'react';
 import Button from '../../components/Button/Button';
-import Modal from '../../components/Modal/Modal';
 
 import './Registration.scss';
 
 class Registration extends Component {
   static displayName = 'Registration';
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isOpen: false
-    };
-  }
-
-  toggleModal = () => {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-
+  
   render() {
-    const modalContentStyle = {
-      padding: '0px',
-      marginTop: '-100px'
-    }
-
     return (
       <div id='main_hero' className=''>
         <div className='container'>
@@ -43,9 +24,7 @@ class Registration extends Component {
             
             <div className='volunteer-section'>
               <h1 className='title'>Volunteer</h1>
-              <div className='tagline'>Interested in being a part of the Kernelcon Crew?</div>
-              <div className='call-to-order'>Use the link below to add yourself to our growing list of Volunteers.  More details to follow in the coming weeks!  In addition to free entry, accepted Volunteers receive guaranteed swag, special Kernelcon Crew t-shirt, and other exclusive gear!</div>
-              <Button onClick={this.toggleModal} href="#" title="Sign Up" class="baltic-sea volunteer-btn"/>
+              <div className='tagline'>Thanks to the overwhelming response Kernelcon is filled up on volunteers for 2019.  We would still love to have you and hope to see you there!</div>
             </div>
 
             <div className='volunteer-section'>
@@ -61,23 +40,6 @@ class Registration extends Component {
               <div className='call-to-order'>Students should submit the above information along with any questions or concerns to <a className='highlight-text' href="mailto:students@kernelcon.org">students@kernelcon.org</a>. Accepted students will receive admission to the conference, a hotel room (for traveling students), and this year's "hacker education kit".</div>
             </div>
 
-            <Modal show={this.state.isOpen}
-              onClose={this.toggleModal}
-              title='Show Your Interest'
-              modalContentStyle={modalContentStyle}
-              height='520'
-              width='500'
-              class='volunteer-modal'>
-                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScokorBVswuxp5TPARSicBpj3aYv-Y1JipodHZEpuWUKCyJ5Q/viewform?embedded=true"
-                  title="volunteer-interest-form"
-                  frameBorder="0"
-                  marginHeight="0"
-                  marginWidth="0"
-                  className="google-form"
-                  style={{'marginLeft': '-30px'}}>
-                  Loading...
-                </iframe>
-            </Modal>
           </div>
         </div>
       </div>
